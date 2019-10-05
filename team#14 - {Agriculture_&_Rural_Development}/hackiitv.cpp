05c234aff4c711a5005852d4c7375718e2510870
Ayush Patel <in.ayushpatel@gmail.com>
2019-10-05 17:07:38 +0530

    4th update

--- team#14 - {Agriculture_&_Rural_Development}/hackiitv.cpp
+++ team#14 - {Agriculture_&_Rural_Development}/hackiitv.cpp
@@ -3,8 +3,80 @@
 
 using namespace std;
 
-int login(){
-	int opt,x,y;
+
+int loginconsumer();
+int loginfarmer();
+int menuconsumer();
+int menufarmer();
+
+
+int main(){
+	int opt;
+	cout<<"Enter 1 for cunsumer login and 2 for farmer login : ";
+	cin>>opt;
+	if(opt==1){
+		loginconsumer();
+	}else if(opt==2){
+		loginfarmer();
+  	}
+}
+
+
+int loginconsumer(){
+	int opt;
+	string user;
+	string pass;
+	vector <string> username;
+	username.push_back("AYUSH");
+	username.push_back("DARSHAN");
+	username.push_back("KESHAV");
+	username.push_back("NITANSHU");
+	username.push_back("ABHAY");
+	vector <string> password;
+	password.push_back("5555");
+	password.push_back("6666");
+	password.push_back("7777");
+	password.push_back("8888");
+	password.push_back("9999");
+	loginhere:
+	cout<<"Enter 1 (login) and 2 (sign up) : ";
+	cin>>opt;
+		if(opt==1){
+			cout<<"enter username : ";
+			cin>>user;
+			cout<<"Enter password : ";
+			cin>>pass;
+			int a;
+			for(int i=0;i<username.size();i++){
+				a=i;
+				if(username[i]==user){
+					break;
+				}
+			}
+			if(password[a]==pass && username[a]==user){
+				cout<<"login successful \n";
+				cout<<"\n \n";
+				menuconsumer();
+			}else{
+				cout<<"Invalid username or password";
+			}
+		}else if(opt==2){
+			cout<<"enter username : ";
+			cin>>user;
+			username.push_back(user);
+			cout<<"Enter password : ";
+			cin>>pass;
+			password.push_back(pass);
+			cout<<"Registration successful : \n";
+			goto loginhere;
+			
+		}
+	
+	}
+
+
+int loginfarmer(){
+	int opt;
 	string user;
 	string pass;
 	vector <string> username;
@@ -54,6 +126,11 @@ int login(){
 	
 	}
 
-int main(){
-	login();
-}
+	int menuconsumer(){
+		
+	}
+	
+	
+	int menufarmer(){
+		
+	}
